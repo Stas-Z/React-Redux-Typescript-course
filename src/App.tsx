6511e4923a0432e7dde59ab7f650b76e39f65f87
Stas-Z <stanislav@zettafoods.ru>
2023-03-10 20:18:55 +0200

    hook usedTypedSelector, async action creator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import UserList from './components/UserList'
+import "./style.css";
+
 
 const App = () => {
   return (
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,38 @@
-import { useSelector } from "react-redux"
+import { Dispatch, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { fetchUsers } from "../store/action-creators/user";
 
 const UserList:React.FC = () => {
-    const state = useSelector(state => state)
-    console.log(state);
+    const { error, loading, users } = useTypedSelector(state => state.user)
+    const dispatch: Dispatch<any> = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+   
+    if (loading){
+        return (<div className="lds-default">
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+        </div>)
+    }
+    if (error){
+        return <h1>{error}</h1>
+    }
     
   return (
-    <div>UserList</div>
+    <div>{users.map(user => <div key={user.id}>{user.name}</div>)}</div>
   )
 }
 
